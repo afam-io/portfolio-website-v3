32e40f969ef30cec38c48a6642300db5fe30e643
AfamOkoh <afamokoh@gmail.com>
2024-02-20 21:59:02 +0000

    style: set star page background

---
+++ src/components/sparkles-background.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React from 'react';
+import { SparklesCore } from './ui/sparkles';
+
+const SparklesBackground = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className='h-screen relative w-screen bg-black flex flex-col items-center justify-center overflow-hidden '>
+      <div className='w-full absolute inset-0 h-screen'>
+        <SparklesCore
+          id='tsparticlesfullpage'
+          background='transparent'
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className='w-full h-full'
+          particleColor='#FFFFFF'
+        />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default SparklesBackground;
